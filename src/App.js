2b8d9700ffd7af0fca1bc6865a8521473ce0785d
snagasuri <ramnag2003@gmail.com>
2023-10-28 17:40:57 -0500

    gosh

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import sampleImage from './sample.jpg';  // Import the image
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={sampleImage} alt="Sample" />  {/* Use the imported image */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi moory :D
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
